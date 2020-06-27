9c8fb3d84dc3046fcd5a6ac01d22a27dcba0e8cc
nonanomalous <hxr@outlook.com>
2020-06-27 14:27:53 -0600

    force Utilization to return range 0-1

--- src/processor.cpp
+++ src/processor.cpp
@@ -14,8 +14,11 @@ float Processor::Utilization() {
 
     long total_delta{total_ticks - Processor::cached_total_ticks_};    
     long active_delta{active_ticks - Processor::cached_active_ticks_};
-    
     cached_total_ticks_ = total_ticks;
     cached_active_ticks_ = active_ticks;
-    return (float)active_delta / (float)total_delta;
+    float util = (float)active_delta / (float)total_delta;
+    if (util > 0.0 && util < 1.0)
+        return util;
+    else
+        return 0.;
     }
